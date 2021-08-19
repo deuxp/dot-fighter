ad9a9f82f27917cb41b614ab17ea8296125e6938
deuxp <deuxp@tutanota.com>
2021-08-18 23:01:22 -0400

    added Kata file

---
+++ Kata/codeWars.js
@@ -0,0 +1,9 @@
+function alphabetPosition(text) {
+    let alist = []
+    for (let i = 0; i < text.length; i++) {
+        //if (text[i].isAlpha()) {
+        // alist.push(text[i])
+        return alist
+    }
+}
+}
